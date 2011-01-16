5ff7ef04bf32d3f250f23fe5730e1131ebfd255c
= <=>
2011-01-16 20:47:48 +0100

    Backend: Batch insert now working, let the inserting begin :)

--- web/admin.py
+++ web/admin.py
@@ -1,6 +1,8 @@
 from web.models import Password, Source
+from web.forms import PasswordsForm
 from django.contrib import admin
 from django.forms import forms
+from django.db import transaction
 
 
 
@@ -8,25 +10,29 @@ admin.site.register(Source)
 
 # TODO: Disable error checking -- create a custom form for the adming...
 class PasswordAdmin(admin.ModelAdmin):
+    form = PasswordsForm
+    
+    @transaction.commit_manually
     def save_model(self, request, obj, form, change):
         if(change == False):
-            # TODO: Find a faster way of doing batch inserts:
-            for pw in request.POST['password'].split('\n'):
+            
+            for pw in request.POST['passwords'].split('\n'):
                 p = Password(password=pw, Source_id=request.POST['Source'])
                 p.save()
         else:
             # Batch insert is not available when editing:
-            obj.password = request.POST['password']
+            obj.password = request.POST['passwords']
             obj.Source_id = request.POST['Source']
             obj.save()
+        transaction.commit()
             
             
     # Change the password input to textarea:
-    # TODO: don't do this if your while in edit mode
-    def formfield_for_dbfield(self, db_field, **kwargs):
-        formfield = super(PasswordAdmin, self).formfield_for_dbfield(db_field, **kwargs)
-        if db_field.name == 'password':
-            formfield.widget = forms.Textarea()
-        return formfield
+    # TODO: don't do this while in edit mode
+#    def formfield_for_dbfield(self, db_field, **kwargs):
+#        formfield = super(PasswordAdmin, self).formfield_for_dbfield(db_field, **kwargs)
+#        if db_field.name == 'password':
+#            formfield.widget = forms.Textarea()
+#        return formfield
 
 admin.site.register(Password, PasswordAdmin)
---
+++ web/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from web.models import Password
+
+class PasswordsForm(forms.ModelForm):
+    class Meta:
+        model = Password
+        exclude = ('password',)
+    passwords = forms.CharField(widget=forms.Textarea(),)
+    
