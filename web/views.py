9804e7e19498ba28492230de25c2e94c786916ce
HairyFotr <maticpotocnik@gmail.com>
2011-01-18 20:12:00 +0100

    Added basic password strength meter

--- web/views.py
+++ web/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.http import HttpResponseRedirect
 
-ERROR_NOT_FOUND = "You have stumbled upon a page that does not exist. Had back to <a href=\"/\">charted waters</a> as soon as you are ready."
+ERROR_NOT_FOUND = "You seem to have stumbled upon a page that does not exist. Head back to <a href=\"/\">charted waters</a> as soon as you are ready."
 ERROR_BAD_REQUEST = "I see what you did there."
 
 def index(request):
@@ -24,11 +24,25 @@ def add(request):
     p.save()
     
     return HttpResponseRedirect('/id/' + str(p.id))
-
+    
+def pass_strength(passwd):
+    # Matic: I should probably put this method elsewhere?
+    # http://stackoverflow.com/questions/75057/what-is-the-best-way-to-check-the-strength-of-a-password
+    print passwd
+    conditions_met = 0
+    if len(passwd) >= 6: 
+        if passwd.lower() != passwd: conditions_met += 1
+        if len([x for x in passwd if x.isdigit()]) > 0: conditions_met += 1
+        if len([x for x in passwd if not x.isalnum()]) > 0: conditions_met += 1
+    
+    return conditions_met
+    
 def id(request, id):
     p = Password.objects.get(pk=id)
     # count() - 1 because the password we are looking for was already entered: 
     count = Password.objects.all().filter(password=p.password).count() - 1
+    #return render_to_response('web/id.html', {'pass_count': count, 'pass_strength': pass_strength(id)},
+    #                                             context_instance=RequestContext(request))
     return render_to_response('web/id.html', {'pass_count': count},
                                                  context_instance=RequestContext(request))
     
