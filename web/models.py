0648ac56f5c0120cececbd2a4952d7e4277aa37a
Smotko <ensmotko@gmail.com>
2011-01-19 09:57:34 +0100

    Moved pass_strength() to the model

--- web/models.py
+++ web/models.py
@@ -6,7 +6,23 @@ class Source(models.Model):
         return self.name
     
 class Password(models.Model):
+    
+    STRONG_PASS_LENGTH = 6
+        
     password = models.CharField(max_length = 1000, blank = True)
     Source = models.ForeignKey(Source)
+        
     def __unicode__(self):
         return self.password + " from " + self.Source.name
+    
+    def pass_strength(self):
+        # Matic: I should probably put this method elsewhere?
+        # Smotko: Yea, this method should be part of the model ;)
+        # http://stackoverflow.com/questions/75057/what-is-the-best-way-to-check-the-strength-of-a-password
+        conditions_met = 0
+        if len(self.password) >= self.STRONG_PASS_LENGTH: 
+            if self.password.lower() != self.password: conditions_met += 1
+            if len([x for x in self.password if x.isdigit()]) > 0: conditions_met += 1
+            if len([x for x in self.password if not x.isalnum()]) > 0: conditions_met += 1
+        
+        return conditions_met
--- web/views.py
+++ web/views.py
@@ -24,23 +24,13 @@ def add(request):
     p.save()
     
     return HttpResponseRedirect('/id/' + str(p.id))
-    
-def pass_strength(passwd):
-    # Matic: I should probably put this method elsewhere?
-    # http://stackoverflow.com/questions/75057/what-is-the-best-way-to-check-the-strength-of-a-password
-    conditions_met = 0
-    if len(passwd) >= 6: 
-        if passwd.lower() != passwd: conditions_met += 1
-        if len([x for x in passwd if x.isdigit()]) > 0: conditions_met += 1
-        if len([x for x in passwd if not x.isalnum()]) > 0: conditions_met += 1
-    
-    return conditions_met
+
     
 def id(request, id):
     p = Password.objects.get(pk=id)
     # count() - 1 because the password we are looking for was already entered: 
     count = Password.objects.all().filter(password=p.password).count() - 1
-    return render_to_response('web/id.html', {'pass_count': count, 'pass_strength': pass_strength(p.password)},
+    return render_to_response('web/id.html', {'pass_count': count, 'pass_strength': p.pass_strength()},
                                                  context_instance=RequestContext(request))    
 def error(request):
     # TODO: Should set status code 404
