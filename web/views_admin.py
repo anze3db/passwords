20f7fae2b8edbeefaa05ae08b63baf1151f838bd
Smotko <ensmotko@gmail.com>
2011-02-02 00:57:11 +0100

    Faster batch password inserting

--- web/views_admin.py
+++ web/views_admin.py
@@ -3,7 +3,7 @@ from web.forms import PasswordsForm
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.http import HttpResponseRedirect, Http404
-from django.db import transaction
+from django.db import connection, transaction
 
 @transaction.commit_manually
 def batch(request):
@@ -13,12 +13,13 @@ def batch(request):
         
         
         # Insert the passwords:
-        
+        cursor = connection.cursor()
         for chunk in request.FILES['passwords']:
-            
-            ps = Password(password=chunk.rstrip('\r\n'), source_id=request.POST['source'])            
-            ps.save(batch = True)            
-    
+            cursor.execute("INSERT INTO web_password (password, source_id, processed) VALUES(%s, " + request.POST['source'] + ", 0)", [chunk.rstrip('\r\n')])
+
+            #ps = Password(password=chunk.rstrip('\r\n'), source_id=request.POST['source'])            
+            #ps.save(batch = True)            
+        transaction.commit_unless_managed()
         # Update source count:                    
         src = Source.objects.get(pk = request.POST['source'])
         src.update_count()
@@ -28,7 +29,7 @@ def batch(request):
         transaction.commit()
 
 
-        return HttpResponseRedirect('/admin/web/password')
+        return HttpResponseRedirect('/admin/web/password/batch')
        
     return render_to_response('admin/web/password/batch.html', {'form': PasswordsForm(),
                                                                 'num_unprocessed': Password.objects.all().filter(processed = False).count()},
